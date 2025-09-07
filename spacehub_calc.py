06f3705b70845084c9e8df1f9e1aff8d72fac069
Nick <ntmarston@gmail.com>
2025-09-06 23:31:18 -0400

    Completed Keplerian orbit elements (steady state)

--- spacehub_calc.py
+++ spacehub_calc.py
@@ -8,31 +8,307 @@ warnings.filterwarnings("ignore")
 class TwoBodyOrbit:
     
     npoints = 0
-    df = None
+    data = None
     i = 0 #Primary mass object
     j = 1 #Secondary mass object
 
+    #Orbit State Vectors
+    time = None
+    R_vec = None
+    V_vec = None
+    magR = None
+    hvec = None
+    e_vec = None
+    N_vec = None
+    magN = None
+
+    eccentricity = None
+    semiMajorAxis = None
+    inclination_rad = None
+    inclination_deg = None
+
+    LongitudeAscendingNode = None
+    LongitudeAscendingNode_deg = None
+    sinOmega = None
+    cosOmega = None
+
+    true_anomaly_rad = None
+    true_anomaly_deg = None
+    sinf = None
+    cosf = None
+
+    argument_of_periapsis_rad = None
+    argument_of_periapsis_deg = None
+
+
     #Setters and Update methods
-    @classmethod
-    def set_data(cls, value):
-        cls.df = value
-
-    @classmethod
-    def set_ij(cls, i, j):
-        cls.i = i
-        cls.j = j
-
-    @classmethod
-    def set_npoints(cls):
-        df = cls.df
-        cls.numpoints = df["time"].nunique()
-
-    def __init__(self, filename, i, j):
-        self.df = load_spacehub_data(filename)
+    
+    def set_data(self, value):
+        self.data = value
+    
+    def set_time(self):
+        col = self.data["time"]
+        self.time = col.drop_duplicates().to_list()
+    
+    def set_ij(self, i, j):
         self.i = i
         self.j = j
-        self.set_npoints()
 
+    
+    def set_npoints(self):
+        df = self.data
+        timesteps = df["time"].nunique()
+        self.npoints = timesteps
+
+    def set_R_and_V(self):
+        data, i, j = self.data, self.i, self.j
+        X, Y, Z = distance(data, 'p', i, j)
+        magR = mag([X, Y, Z])
+        self.R_vec = [X, Y, Z]
+        self.magR = magR
+
+        vx, vy, vz = distance(data, 'v', i, j)
+        self.V_vec = [vx, vy, vz]
+
+    def set_h_vector(self):
+        i = self.i
+        j = self.j
+        data = self.data
+        dx, dy, dz = distance(data, 'p', i, j)
+        dvx, dvy, dvz = distance(data, 'v', i, j)
+        self.hvec = calc_h_vector([dx, dy, dz], [dvx, dvy, dvz])
+
+    
+    def set_ecc_vector(self):
+        i = self.i
+        j = self.j
+        data = self.data
+        mi = get_tot_mass(data, i)
+        mj = get_tot_mass(data, j)
+        dx, dy, dz = distance(data, 'p', i, j)
+        dvx, dvy, dvz = distance(data, 'v', i, j)
+
+        ecx, ecy, ecz = [], [], []
+        for t in range(0, len(dx)):
+            ex, ey, ez = calc_ecc(mi + mj, dx[t], dy[t], dz[t], dvx[t], dvy[t], dvz[t])
+            ecx.append(ex)
+            ecy.append(ey)
+            ecz.append(ez)
+        
+        self.e_vec = [ecx, ecy, ecz]
+    
+    def set_N_vector(self):
+        i = self.i
+        j = self.j
+        data = self.data
+        nxs, nys, nzs = [], [], []
+        N = []
+        for t in range(0, self.npoints):
+            r = [self.R_vec[0][t], self.R_vec[1][t], self.R_vec[2][t]]
+            v = [self.V_vec[0][t], self.V_vec[1][t], self.V_vec[2][t]]
+            h = np.cross(r,v)
+            khat = [0,0,1]
+            kcrossh = np.cross(khat, h)
+            nx, ny, nz = kcrossh[0], kcrossh[1], kcrossh[2]
+            nxs.append(nx)
+            nys.append(ny)
+            nzs.append(nz)
+            N.append(mag([nx, ny, nz]))
+
+        self.N_vec = [nxs, nys, nzs]
+        self.magN = N
+
+    def set_sma(self):
+        i = self.i
+        j = self.j
+        data = self.data
+        #Nearly Eq. 2.134 of Murray-Dermott
+        mi = get_tot_mass(data, i)
+        mj = get_tot_mass(data, j)
+        dx, dy, dz = distance(data, 'p', i, j)
+        dvx, dvy, dvz = distance(data, 'v', i, j)
+
+        smas = []
+        for t in range(0, len(dx)):
+            a = calc_sma(mi + mj, dx[t], dy[t], dz[t], dvx[t], dvy[t], dvz[t])
+            smas.append(a)
+
+        self.semiMajorAxis = smas
+
+    
+    def set_scalar_e(self):
+        i = self.i
+        j = self.j
+        data = self.data
+        #Eq 2.135 of Murray-Dermott
+        mi = get_tot_mass(data, i)
+        mj = get_tot_mass(data, j)
+        mu = (mi+mj) #* G.value
+        hvec = self.hvec
+        h = mag(hvec)
+        a = self.semiMajorAxis
+
+        e = []
+
+        for t in range(0, len(a)):
+            e.append(np.sqrt(1-(h[t]**2)/(mu * a[t])))
+        
+        self.eccentricity = e
+
+   
+    def set_inclination(self):
+        i = self.i
+        j = self.j
+        data = self.data
+        #Eq 2.134 of Murray-Dermott
+        mi = get_tot_mass(data, i)
+        mj = get_tot_mass(data, j)
+        mu = (mi+mj) #* G.value
+        dx, dy, dz = distance(data, 'p', i, j)
+        dvx, dvy, dvz = distance(data, 'v', i, j)
+        hvec = calc_h_vector([dx, dy, dz], [dvx, dvy, dvz])
+        h = mag(hvec)
+        I = []
+        for t in range(0, len(h)):
+            I.append(np.arccos(hvec[2][t]/h[t]))
+        
+        self.inclination_rad = I
+        self.inclination_deg = np.rad2deg(I)
+
+    
+    def set_longitude_of_ascending_node(self):
+        i = self.i
+        j = self.j
+        data = self.data
+
+        #returns Omega, sinOmega, cosOmega
+        mi = get_tot_mass(data, i)
+        mj = get_tot_mass(data, j)
+        mu = (mi+mj) #* G.value
+        hvec = self.hvec
+        h = mag(hvec)
+        incl = self.inclination_rad
+
+    
+
+        sines = []
+        cosines = []
+        Omegas = []
+        for t in range(0, self.npoints):
+            if hvec[2][t] > 0:
+                hx = hvec[0][t]
+                hy = -1 * hvec[1][t]
+            else:
+                hx = -1 * hvec[0][t]
+                hy = hvec[1][t]
+
+        
+            
+                
+            sines.append(hx / (h[t]*np.sin(incl[t])))
+            cosines.append(hy / (h[t]*np.sin(incl[t])))
+
+            Omega_t = np.arcsin(hx / (h[t]*np.sin(incl[t])))
+            if Omega_t < 0:
+                Omega_t = 2*np.pi + Omega_t
+            Omegas.append(Omega_t)
+            
+
+        #checkpoint
+        for t in range(0, self.npoints):
+            checksum = sines[t]**2 + cosines[t]**2
+            assert np.abs(1-checksum) < 0.1, f"Checkpoint test failed in set_longitude_of_ascending_node. sin^2+cos^2 = {checksum}"
+        
+        self.sinOmega = sines
+        self.cosOmega = cosines
+        self.LongitudeAscendingNode = Omegas
+        self.LongitudeAscendingNode_deg = [np.rad2deg(o) for o in Omegas]
+    
+    def set_true_anomaly(self):
+        #Could not make the equations in Murray-Dermott cooperate with the way numpy trig functions work,
+        #So I am using the eccentricity vector to calculate it
+        evec = self.e_vec
+        rvec = self.R_vec
+        vvec = self.V_vec
+        f = []
+        fdeg = []
+        cosf = []
+        sinf = []
+        for t in range(0, self.npoints):
+            e = [evec[0][t], evec[1][t], evec[2][t]]
+            r = [rvec[0][t], rvec[1][t], rvec[2][t]]
+            v = [vvec[0][t], vvec[1][t], vvec[2][t]]
+            edotr = scalar_product_xyz(e, r)
+            mager = mag(e) * mag(r)
+            rdotv = scalar_product_xyz(r, v)
+            cosft = edotr/mager
+            cosf.append(cosft)
+            if rdotv > 0:
+                f.append(np.arccos(cosft))
+                fdeg.append(np.rad2deg(np.arccos(cosft)))
+            else:
+                f.append(2*np.pi - np.arccos(cosft))
+                fdeg.append(np.rad2deg(2*np.pi - np.arccos(cosft)))
+            sinf.append(np.sqrt(1-cosft**2))
+        
+        self.true_anomaly_rad = f
+        self.true_anomaly_deg = fdeg
+        self.sinf = sinf
+        self.cosf = cosf
+    
+    def set_argument_of_periapsis(self):
+        evec = self.e_vec
+        escalars = self.eccentricity
+        peri = []
+        perideg = []
+        nvec = self.N_vec
+        N = self.magN
+        for t in range(0, self.npoints):
+            Nv = [nvec[0][t], nvec[1][t], nvec[2][t]]
+            e_vec = [evec[0][t], evec[1][t], evec[2][t]]
+            e = escalars[t]
+            Nt = N[t]
+            if e_vec[2] >= 0:
+                omega = np.arccos(np.dot(Nv, e_vec) / (Nt * e))
+            else:
+                omega = 2 * np.pi - np.arccos(np.dot(Nv, e_vec) / (Nt * e))
+            peri.append(omega)
+            perideg.append(np.rad2deg(omega))
+
+        self.argument_of_periapsis_rad = peri
+        self.argument_of_periapsis_deg = perideg
+
+
+    def __init__(self, filename, i, j):
+        self.data = load_spacehub_data(filename)
+        self.i = i
+        self.j = j
+        #set len and time
+        self.set_npoints()
+        self.set_time()
+        #set R vec
+        self.set_R_and_V()
+        #set hvec
+        self.set_h_vector()
+        #set N vec
+        self.set_N_vector()
+        #solve eccentricity vector
+        self.set_ecc_vector()
+        #solve Semi-major axis
+        self.set_sma()
+        #solve eccentricity
+        self.set_scalar_e()
+        #solve inclination
+        self.set_inclination()
+        #solve longitude of ascending node
+        self.set_longitude_of_ascending_node()
+        #solve true anomaly
+        self.set_true_anomaly()
+        #solve argument of periapsis
+        self.set_argument_of_periapsis()
+
+
+        
 
 
 #-----Helper/Standalone Functions Below-----
@@ -122,21 +398,17 @@ def calc_h_vector(r, v):
 
     return hx, hy, hz
 
-#-----------Getter/Modifier functions----------
-#
-#------------Helpers/Intermediates---------------
-
-
-
-def get_h_vector(data, i, j):
-    dx, dy, dz = distance(data, 'p', i, j)
-    dvx, dvy, dvz = distance(data, 'v', i, j)
-    hvec = calc_h_vector([dx, dy, dz], [dvx, dvy, dvz])
-
-    return hvec
-
 def get_tot_mass(data, tup):
-    #get total mass of the system
+    """Get the total mass of one or more particles (passed as tuple or int)
+
+    Args:
+        data (pd.DataFrame): The spacehub output dataframe created by load_spacehub_data
+        param2 (int or tuple): The index/indices of the particles to consider
+
+    Returns:
+        float: The total mass of the particle(s)
+
+    """
     if type(tup) is int:
         return data[data["id"]==tup]["mass"][tup]
     else:
@@ -145,7 +417,7 @@ def get_tot_mass(data, tup):
         for t in tup:
             mtot += data[data["id"]==t]["mass"][t]
         return mtot
-
+    
 def get_com(data, key, tup):
     #get center of mass
     mt = get_tot_mass(data, tup)
@@ -161,6 +433,34 @@ def get_com(data, key, tup):
 
     return x/mt, y/mt, z/mt
 
+def mag(vec):
+    if isinstance(vec[0], numbers.Number):
+        square = 0
+        for a in vec:
+            square += a**2
+
+        return np.sqrt(square)
+
+    mags = []
+    for t in range(0, len(vec[0])):
+        square = vec[0][t]**2 + vec[1][t]**2 + vec[2][t]**2
+        
+
+        mags.append(np.sqrt(square))
+
+    return mags
+#-----------Getter/Modifier functions----------
+#
+#------------Helpers/Intermediates---------------
+
+def get_h_vector(data, i, j):
+    dx, dy, dz = distance(data, 'p', i, j)
+    dvx, dvy, dvz = distance(data, 'v', i, j)
+    hvec = calc_h_vector([dx, dy, dz], [dvx, dvy, dvz])
+
+    return hvec
+
+
 def add_norms(data):
     #adds norm column to output, is inplace
     p_num = data["id"].nunique()
@@ -174,6 +474,10 @@ def add_norms(data):
         vy = data['vy']
         vz = data['vz']
         data['v'] = calc_norm(vx, vy, vz)
+
+def scalar_product_xyz(vec1, vec2):
+    #does not do lists
+    return vec1[0]*vec2[0] + vec1[1]*vec2[1] + vec1[2]*vec2[2]
 
 #---------Outputs/Keplerian Elements-----------
 
@@ -192,22 +496,7 @@ def get_L(data, i, j):
 
     return Lx, Ly, Lz
 
-def mag(vec):
-    if isinstance(vec[0], numbers.Number):
-        square = 0
-        for a in vec:
-            square += a**2
 
-        return np.sqrt(square)
-
-    mags = []
-    for t in range(0, len(vec[0])):
-        square = vec[0][t]**2 + vec[1][t]**2 + vec[2][t]**2
-        
-
-        mags.append(np.sqrt(square))
-
-    return mags
 
 #---Keplerian orbital elements---
 # Size and Shape
